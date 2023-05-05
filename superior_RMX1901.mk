445fa0972572875f6ab89333ada951b33457cdc9
Shravankumar <shravankumarsayz@gmail.com>
2023-05-05 10:40:52 +0530

    Update and rename aosp_RMX1901.mk to superior_RMX1901.mk

--- superior_RMX1901.mk
+++ superior_RMX1901.mk
@@ -1,6 +1,4 @@
 #
-# Copyright (C) 2020 The LineageOS Project
-#
 # SPDX-License-Identifier: Apache-2.0
 #
 
@@ -9,19 +7,17 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 
-# Inherit some common Project-Elixir stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit some common SuperiorOs stuff.
+$(call inherit-product, vendor/superior/config/common.mk)
 
 TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_WITH_GAPPS := true
+BUILD_WITH_GAPPS := true
 BUILD_TARGET_WANTS_FOD_ANIMATIONS := true
 TARGET_USES_BLUR := true
 TARGET_FACE_UNLOCK_SUPPORTED := true
-ELIXIR_BUILD_TYPE := UNOFFICIAL
-ELIXIR_MAINTAINER := SHRAVAN
+DEVICE_MAINTAINER := shravan
 EXTRA_UDFPS_ANIMATIONS := true
-TARGET_HAS_UDFPS := true
-
+SUPERIOR_UDFPS_ANIMATIONS := true
 
 # Inherit from RMX1901 device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
@@ -29,17 +25,17 @@ $(call inherit-product, $(LOCAL_PATH)/device.mk)
 PRODUCT_BRAND := Realme
 PRODUCT_DEVICE := RMX1901
 PRODUCT_MANUFACTURER := Realme
-PRODUCT_NAME := aosp_RMX1901
+PRODUCT_NAME := superior_RMX1901
 PRODUCT_MODEL := Realme X
 
 PRODUCT_GMS_CLIENTID_BASE := android-oppo
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="cheetah-user 13 TQ2A.230405.003.E1 9802792 release-keys" \ 
-    PRODUCT_NAME="RMX1901"
+    PRIVATE_BUILD_DESC="cheetah-user 13 TQ2A.230405.002 9891397 release-keys" \ 
+    PRODUCT_NAME=RMX1901
     
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := google/cheetah/cheetah:13/TQ2A.230405.003.E1/9802792:user/release-keys
+BUILD_FINGERPRINT := google/cheetah/cheetah:13/TQ2A.230505.002/9891397:user/release-keys
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.build.fingerprint=$(BUILD_FINGERPRINT)
