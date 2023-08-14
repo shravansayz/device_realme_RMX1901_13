faa885d478b4717061979e73585ac90e6c82d2e9
Shravankumar <shravankumarsayz@gmail.com>
2023-08-14 14:05:19 +0530

    RMX1901: Drop Dolby

--- prop/vendor_prop.mk
+++ prop/vendor_prop.mk
@@ -125,16 +125,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.display.cabl=2 \
     vendor.display.disable_rotator_downscale=1
 
-# Dolby
-PRODUCT_PROPERTY_OVERRIDES += \
-    vendor.audio.dolby.ds2.enabled=false \
-    vendor.audio.dolby.ds2.hardbypass=false \
-    ro.vendor.dolby.dax.version=DAX3_3.6.0.12_r1 \
-    ro.vendor.product.device.db=OP_DEVICE \
-    ro.vendor.product.manufacturer.db=OP_PHONE \
-    vendor.product.device=OP_PHONE \
-    vendor.product.manufacturer=OPD
-
 # DRM
 PRODUCT_PROPERTY_OVERRIDES += \
     drm.service.enabled=true
