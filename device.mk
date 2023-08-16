4fccb1163f004cd49d0b0292564b9d464d478a32
shravansayz <shravankumarsayz@gmail.com>
2023-08-16 13:09:41 +0530

    RMX1901: Bump qti.hardware.bluetooth_audio to v2.1 Signed-off-by: Dakkshesh <dakkshesh5@gmail.com>

--- device.mk
+++ device.mk
@@ -34,7 +34,7 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth.audio-impl \
     audio.bluetooth.default \
     libbthost_if \
-    vendor.qti.hardware.bluetooth_audio@2.0.vendor \
+    vendor.qti.hardware.bluetooth_audio@2.1.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor
 
 # Audio
