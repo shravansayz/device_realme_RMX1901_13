4f3b90f3ea31b64464cd5eb951c9377457e8a429
shravansayz <shravankumarsayz@gmail.com>
2023-08-18 14:24:10 +0530

    RMX1901: PEX Bringup!!!!!
    Signed-off-by: shravansayz <shravankumarsayz@gmail.com>

--- aosp_RMX1901.mk
+++ aosp_RMX1901.mk
@@ -9,43 +9,20 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 
-# Inherit some common Rising stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common PEX stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
-# maintainer flag (one word only/no spacing)
-RISING_MAINTAINER := Shravan
-
-# chipset flag (one word only/no spacing)
-RISING_CHIPSET := sdm710
-
-# gapps build flag, if not defined build type is vanilla
-WITH_GMS := true
-TARGET_CORE_GMS := false
-
-# Sushi Bootanimation (only 720/1080p/1440 supported. if not defined, bootanimation is google bootanimation)
-SUSHI_BOOTANIMATION := 1080
-
-# disable/enable blur support, default is false
+TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_ENABLE_BLUR := true
-
-# UDFPS ICONS/ANIMATIONS
 TARGET_HAS_UDFPS := true
-
-
-# Aperture Camera (default: not defined - skipped by the compiler)
-TARGET_EXCLUDES_APERTURE := true
-
-# Wether to use google (true) or AOSP (false) telephony package bundle. (defaults: false for gms core, true for pixel builds)
-TARGET_USE_GOOGLE_TELEPHONY := true
-
+EXTRA_UDFPS_ANIMATIONS := true
 TARGET_SUPPORTS_NEXT_GEN_ASSISTANT := true
-TARGET_EXCLUDES_SEEDVAULT := true
-TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_GAPPS_ARCH := arm64
 TARGET_INCLUDE_LIVE_WALLPAPERS := true
 TARGET_SUPPORTS_QUICK_TAP := true
+PEX_BUILD_TYPE := UNOFFICIAL
+TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_USES_BLUR := true
-TARGET_EXCLUDES_AUDIOFX := true
-
 
 # Inherit from RMX1901 device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
@@ -53,7 +30,7 @@ $(call inherit-product, $(LOCAL_PATH)/device.mk)
 PRODUCT_BRAND := Realme
 PRODUCT_DEVICE := RMX1901
 PRODUCT_MANUFACTURER := Realme
-PRODUCT_NAME := lineage_RMX1901
+PRODUCT_NAME := aosp_RMX1901
 PRODUCT_MODEL := Realme X
 
 PRODUCT_GMS_CLIENTID_BASE := android-oppo
